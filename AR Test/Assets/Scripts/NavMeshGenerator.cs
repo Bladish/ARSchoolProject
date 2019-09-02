7e030dcef4d51c82613322d97b1e83173b02f0b5
Jonathan Aronsson Olsson <jonte962010@hotmail.com>
2019-09-02 15:00:03 +0200

    First Push

---
+++ AR Test/Assets/Scripts/NavMeshGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshGenerator : MonoBehaviour
+{
+    private NavMeshSurface navMeshSurface;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        //navMeshSurface.BuildNavMesh();
+    }
+
+
+}
+
---
+++ AR Test/Assets/Scripts/ResolutionManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionManager : MonoBehaviour
+{
+    public int startWidth, startHeight, targetWidth, targetHeight; 
+
+    void SetResolution()
+    {
+        Screen.SetResolution(targetWidth, targetHeight, false);
+    }
+    private void Start()
+    {
+        SetResolution();
+    }
+}
